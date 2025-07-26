70472bfd6273f691e10b0f08c6602a217249f849
Jobally Forde <76573103+jobaford@users.noreply.github.com>
2025-07-26 10:36:01 -0400

    Add files via upload

---
+++ Kotlin/DataTypes/Numbers/Float/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val myNumber = 2.5F //this is a float
+    val mySecondNumber = 2.0 // this is a double
+
+    println("myNumber = $myNumber and mySecondNumber = $mySecondNumber")
+}
