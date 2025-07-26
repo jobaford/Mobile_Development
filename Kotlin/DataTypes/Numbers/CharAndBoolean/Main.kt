d5d96bcedb10d9038b52d8b9d5de266ca50a4412
Jobally Forde <76573103+jobaford@users.noreply.github.com>
2025-07-26 11:38:42 -0400

    Add files via upload

---
+++ Kotlin/DataTypes/Numbers/CharAndBoolean/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val myCharValue: Char = 'J'
+    val myBooleanValue: Boolean = true
+    println(myCharValue)
+    println(myBooleanValue)
+}
