60beee98c2be38bbf6460077e795ba679bdbe122
Jobally Forde <76573103+jobaford@users.noreply.github.com>
2025-08-12 02:30:48 -0400

    Add files via upload

---
+++ Kotlin/OOP/Singleton/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    
+    println(Database)
+}
+
+object Database{
+    init {
+        println("Database created")
+    }
+}
